b744a59f0ad28449c1e5c7610d3ae9dbf8354cc3
xogus3492 <wkdxogus3492@gmail.com>
2025-04-07 21:49:44 +0900

    chore: after-deploy.sh 스크립트 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,16 +7,14 @@ if [ -f "docker-compose.dev.yml" ]; then
   echo "> Stop & Remove docker services. (dev)"
   cd ..
   docker compose -f docker-compose.dev.yml down
+
+  echo "> Run new docker services. (dev)"
+    docker compose -f docker-compose.dev.yml up --build -d
 else
   echo "> Stop & Remove docker services. (prod)"
   cd ..
   docker compose -f docker-compose.prod.yml down
-fi
 
-if [ -f "docker-compose.dev.yml" ]; then
-  echo "> Run new docker services. (dev)"
-  docker compose -f docker-compose.dev.yml up --build -d
-else
   echo "> Run new docker services. (prod)"
   docker compose -f docker-compose.prod.yml up --build -d
 fi
