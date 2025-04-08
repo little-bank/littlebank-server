6c7fb463e7a0dc0c3dd7a4f328d1204f8a77dffe
xogus3492 <wkdxogus3492@gmail.com>
2025-04-08 23:24:57 +0900

    chore: CI/CD 테스트

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,14 +5,12 @@ cd $REPOSITORY/littlebank
 
 if [ -f "docker-compose.dev.yml" ]; then
   echo "> Stop & Remove docker services. (dev)"
-  cd ..
   docker compose -f docker-compose.dev.yml down
 
   echo "> Run new docker services. (dev)"
     docker compose -f docker-compose.dev.yml up --build -d
 else
   echo "> Stop & Remove docker services. (prod)"
-  cd ..
   docker compose -f docker-compose.prod.yml down
 
   echo "> Run new docker services. (prod)"
