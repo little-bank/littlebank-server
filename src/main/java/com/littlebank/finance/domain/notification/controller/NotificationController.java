070577366a86c120a071f8d93b4202cf85c463f7
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-06-24 00:00:48 +0900

    feat: notification 페이징 처리

--- src/main/java/com/littlebank/finance/domain/notification/controller/NotificationController.java
+++ src/main/java/com/littlebank/finance/domain/notification/controller/NotificationController.java
@@ -2,23 +2,19 @@ package com.littlebank.finance.domain.notification.controller;
 
 import com.littlebank.finance.domain.notification.dto.response.NotificationResponseDto;
 import com.littlebank.finance.domain.notification.service.NotificationService;
+import com.littlebank.finance.global.common.CustomPageResponse;
+import com.littlebank.finance.global.common.PaginationPolicy;
 import com.littlebank.finance.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api-user/feed/notification")
@@ -31,10 +27,12 @@ public class NotificationController {
     @Operation(summary = "피드 알림 API")
     @SecurityRequirements()
     @GetMapping
-    public ResponseEntity<Page<NotificationResponseDto>> getNotifications(
+    public ResponseEntity<CustomPageResponse<NotificationResponseDto>> getNotifications(
             @AuthenticationPrincipal CustomUserDetails currentUser,
-            @PageableDefault(size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<NotificationResponseDto> response = notificationService.getUserNotifications(currentUser.getId(), pageable);
+            @RequestParam(name = "pageNumber") Integer pageNumber
+    ) {
+        Pageable pageable = PageRequest.of(pageNumber, PaginationPolicy.GENERAL_PAGE_SIZE);
+        CustomPageResponse<NotificationResponseDto> response = notificationService.getUserNotifications(currentUser.getId(), pageable);
         return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/littlebank/finance/domain/notification/domain/repository/CustomNotificationRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.littlebank.finance.domain.notification.domain.repository;
+
+import com.littlebank.finance.domain.notification.domain.QNotification;
+import com.littlebank.finance.domain.notification.dto.response.NotificationResponseDto;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static com.littlebank.finance.domain.notification.domain.QNotification.notification;
+
+@RequiredArgsConstructor
+public class CustomNotificationRepositoryImpl implements CustomNotificationRepository {
+    private final JPAQueryFactory queryFactory;
+    private QNotification n =notification;
+
+    @Override
+    public Page<NotificationResponseDto> findByReceiverOrderByCreatedDateDesc(Long userId, Pageable pageable) {
+        List<NotificationResponseDto> results = queryFactory
+                .select(
+                        Projections.constructor(
+                                NotificationResponseDto.class,
+                                n.id,
+                                n.message,
+                                n.type,
+                                n.createdDate,
+                                n.isRead
+                        )
+                )
+                .from(n)
+                .where(n.receiver.id.eq(userId))
+                .orderBy(n.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        return new PageImpl<>(results, pageable, results.size());
+
+    }
+}
